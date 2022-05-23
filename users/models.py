64dc9d34edb96e2e1927d0413acb9b18bbd932ce
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-23 19:47:44 +0430

    UPDATE

--- users/models.py
+++ users/models.py
@@ -12,6 +12,26 @@ class User(AbstractUser):
         (GENDER_FEMALE, "FEMALE"),
         (GENDER_OTHER, "OTHER")
     )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_TURKISH = "tu"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_TURKISH, "Turkish")
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_TRY = "try"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_TRY, "TRY")
+    )
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
-    bio = models.TextField(default="",blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
