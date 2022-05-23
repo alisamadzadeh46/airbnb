bc9bcb5584a7c516174697bcf84a408a00859f7b
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-23 19:22:28 +0430

    UPDATE

--- users/models.py
+++ users/models.py
@@ -1,7 +1,17 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
 
+    GENDER_CHOICES = (
+        (GENDER_MALE, "male"),
+        (GENDER_FEMALE, "FEMALE"),
+        (GENDER_OTHER, "OTHER")
+    )
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    bio = models.TextField(default="",blank=True)
