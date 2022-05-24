c3a27ad705a7acbf0ff538db5a95dd597c30ea74
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-24 19:51:31 +0430

    UPDATE

--- airbnb/settings.py
+++ airbnb/settings.py
@@ -37,6 +37,10 @@ DJANGO_APPS = [
     'django.contrib.staticfiles',
 ]
 
+THIRD_PARTY_APPS = [
+
+]
+
 PROJECT_APPS = [
     'conversations.apps.ConversationsConfig',
     'lists.apps.ListsConfig',
@@ -47,7 +51,7 @@ PROJECT_APPS = [
     'core.apps.CoreConfig',
 ]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
