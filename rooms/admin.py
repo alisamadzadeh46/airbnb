90ff17dad561b795967227a84f256a7966a633ab
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-25 19:32:23 +0430

    UPDATE

--- rooms/admin.py
+++ rooms/admin.py
@@ -9,6 +9,17 @@ class ItemAdmin(admin.ModelAdmin):
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    fields = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")}
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")}
+        )
+
+    )
     list_display = (
         "name",
         "country",
@@ -22,10 +33,12 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book")
-    list_filter = ("city", "country", "instant_book")
+    list_filter = (
+        "host__superhost", "city", "country", "instant_book", "room_type", "amenities", "facilities", "house_rules")
     search_fields = ("=city", "^host__username")
     search_help_text = "search city and host"
     list_per_page = 5
+    filter_horizontal = ("amenities", "facilities", "house_rules")
 
 
 @admin.register(Photo)
