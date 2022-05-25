37b710949e095a07dfc8e340e6b9d1eb12f7ce44
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-25 19:46:44 +0430

    UPDATE

--- rooms/admin.py
+++ rooms/admin.py
@@ -9,36 +9,42 @@ class ItemAdmin(admin.ModelAdmin):
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    fields = (
+    fieldsets = (
         (
             "Basic Info",
             {"fields": ("name", "description", "country", "address", "price")}
         ),
-        (
-            "Times",
-            {"fields": ("check_in", "check_out", "instant_book")}
-        )
 
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        (
+            "More About the Space", {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules")
+            }),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        ("Last Details", {"fields": ("host",)}),
     )
-    list_display = (
-        "name",
-        "country",
-        "city",
-        "price",
-        "price",
-        "guests",
-        "beds",
-        "bedrooms",
-        "baths",
-        "check_in",
-        "check_out",
-        "instant_book")
-    list_filter = (
-        "host__superhost", "city", "country", "instant_book", "room_type", "amenities", "facilities", "house_rules")
-    search_fields = ("=city", "^host__username")
-    search_help_text = "search city and host"
-    list_per_page = 5
-    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+
+list_display = (
+    "name",
+    "country",
+    "city",
+    "price",
+    "price",
+    "guests",
+    "beds",
+    "bedrooms",
+    "baths",
+    "check_in",
+    "check_out",
+    "instant_book")
+list_filter = (
+    "host__superhost", "city", "country", "instant_book", "room_type", "amenities", "facilities", "house_rules")
+search_fields = ("=city", "^host__username")
+search_help_text = "search city and host"
+list_per_page = 5
+filter_horizontal = ("amenities", "facilities", "house_rules")
 
 
 @admin.register(Photo)
