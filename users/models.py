52fc2bd3331783311a839d2da7615123e6e2a7ea
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-26 21:44:37 +0430

    UPDATE

--- users/models.py
+++ users/models.py
@@ -29,7 +29,7 @@ class User(AbstractUser):
         (CURRENCY_TRY, "TRY")
     )
 
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
