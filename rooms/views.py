8321d37f3a3cbf3e57656669b21fb3c9967639fd
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-27 17:53:44 +0430

    UPDATE

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from . import models
 
 
 def all_rooms(request):
-    rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    rooms = models.Room.objects.all()[offset:limit]
     data = {
         'rooms': rooms
     }
