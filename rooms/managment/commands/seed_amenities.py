b3aec477ec77d7728f0473f2f55034e3c2a56ffe
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-27 10:55:38 +0430

    UPDATE

---
+++ rooms/managment/commands/seed_amenities.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+from rooms.models import Amenity
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        amenities = [
+            "Air conditioning",
+            "Alarm clock",
+            "Balcony",
+            "Bathroom"
+        ]
+        for a in amenities:
+            Amenity.objects.create(name=a)
+        self.stdout.write(self.style.SUCCESS("Amenities created!"))
