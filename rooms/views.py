eedea2bb872ff8ec7488830a3b4eec9cb65882a1
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-27 15:06:31 +0430

    UPDATE

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import models
 
 
 def all_rooms(request):
-    return HttpResponse(content="Hi")
+    rooms = models.Room.objects.all()
+    data = {
+        'rooms': rooms
+    }
+    return render(request, "rooms/home.html", data)
