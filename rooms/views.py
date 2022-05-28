3b98b9745a3294f5692cb4206968001e5d18031e
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-05-28 18:19:42 +0430

    UPDATE

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage
 from . import models
+from django.views.generic import ListView
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-
-    try:
-        rooms = paginator.get_page(int(page))
-        return render(request, "rooms/home.html", {"rooms": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+    model = models.Room
