e6f9f111c7232460fa8343d89558a0b0f9f7f2f4
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-06-04 00:46:20 +0430

    update

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("users", views.LoginView.as_view(), name="users"),
+    path("login", views.LoginView.as_view(), name="login"),
     path("signup", views.SignUpView.as_view(), name="signup"),
     path("verify/<str:key>", views.complete_verification, name="complete_verification"),
     path("login/github", views.github_login, name="github_login"),
