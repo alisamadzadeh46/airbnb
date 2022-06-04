4c7458056b39fa26ed4621e1fc0262f15c8311cd
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-06-04 12:18:28 +0430

    update

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("verify/<str:key>", views.complete_verification, name="complete_verification"),
     path("login/github", views.github_login, name="github_login"),
     path("login/github/callback", views.github_callback, name="github_callback"),
+    path("login/kakao", views.kakao_login, name="kakao_login"),
+    path("login/kakao/callback", views.kakao_callback, name="kakao_callback"),
 ]
