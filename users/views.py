29270139558a5fa2fe4e00e95a41537a5c9a8d2d
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-06-04 12:35:06 +0430

    update

--- users/views.py
+++ users/views.py
@@ -115,10 +115,20 @@ def github_callback(request):
 
 
 def kakao_login(request):
-    app_key = os.environ.get("K_KEY")
+    client_id = os.environ.get("KAKAO_ID")
     redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
     return redirect(
-        f"https://kauth.kakao.com//oauth/authorize?client_id={app_key}&redirect_uri=${redirect_uri}&response_type=code")
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code")
+
+
+class KakaoException(Exception):
+    pass
+
 
 def kakao_callback(request):
-    pass
+    try:
+        code = request.GET.get("code")
+        client_id = os.environ.get("KAKAO_ID")
+        token_request = requests.get(f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}")
+    except KakaoException:
+        return redirect(reverse("users:login"))
