85dec58464ab3faa54035baf436e5969a72cce19
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-06-04 14:01:26 +0430

    update

--- users/views.py
+++ users/views.py
@@ -132,5 +132,17 @@ def kakao_callback(request):
         redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
         token_request = requests.get(
             f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}")
+        token_json = token_request.json()
+        error = token_json.get("error", None)
+        if error is not None:
+            raise KakaoException()
+        access_token = token_json.get("access_token")
+        profile_request = requests.get("https://kapi.kakao.com/v1/user/me", headers={
+            'Authorization': f"Bearer{access_token}"
+        }, )
+        profile_json = profile_request.json()
+        email = profile_json.get("kaccount_email")
+        if email is None:
+            raise KakaoException()
     except KakaoException:
         return redirect(reverse("users:login"))
