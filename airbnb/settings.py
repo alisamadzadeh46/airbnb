30264808f82e491c3f3253273738526685637872
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-06-06 18:37:57 +0430

    update

--- airbnb/settings.py
+++ airbnb/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'static'
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
